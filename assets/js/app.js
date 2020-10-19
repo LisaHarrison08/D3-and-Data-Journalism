05471ac8af273aa83136da3c41d59849311ba353
Lisa Harrison <lmharrison0810@gmail.com>
2020-10-18 21:07:25 -0400

    Initial code added for svg chart

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,50 @@
-// @TODO: YOUR CODE HERE!
+// Data Journalism and D3
+
+// The code for the chart is wrapped inside a function
+// that automatically resizes the chart
+function makeResponsive() {
+
+    // if the SVG area isn't empty when the browser loads, remove it
+    // and replace it with a resized version of the chart
+    var svgArea = d3.select("body").select("svg");
+    if (!svgArea.empty()) {
+      svgArea.remove();
+    }
+  
+    // SVG wrapper dimensions are determined by the current width
+    // and height of the browser window.
+    var svgWidth = window.innerWidth;
+    var svgHeight = window.innerHeight;
+    
+    // Set SVG Margins - To Be Adjusted once graph is populated!
+    var margin = {
+      top: 50,
+      right: 50,
+      bottom: 50,
+      left: 50
+    };
+  
+    var height = svgHeight - margin.top - margin.bottom;
+    var width = svgWidth - margin.left - margin.right;
+
+      // append svg and group
+    var svg = d3.select("#scatter")
+    .append("svg")
+    .attr("height", svgHeight)
+    .attr("width", svgWidth);
+
+    var chartGroup = svg.append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+
+
+// Import Data
+
+
+}
+
+// When the browser loads, makeResponsive() is called.
+makeResponsive();
+
+// When the browser window is resized, responsify() is called.
+d3.select(window).on("resize", makeResponsive);
